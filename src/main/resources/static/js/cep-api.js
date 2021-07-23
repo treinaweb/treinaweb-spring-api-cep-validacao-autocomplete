5ee075e96a7f7d4425803ca8119268accb791f9d
Cleyson Lima <cleysonph@gmail.com>
2021-07-23 02:11:51 -0300

    03:06 - autocomplete do formulário

--- src/main/resources/static/js/cep-api.js
+++ src/main/resources/static/js/cep-api.js
@@ -1,1 +1,24 @@
 $("#cep").mask("00000-000");
+
+let cep = document.getElementById("cep");
+
+cep.addEventListener("keyup", function (event) {
+  if (cep.value.length == 9) {
+    autoComplete(cep.value);
+  }
+});
+
+function autoComplete(cep) {
+  let url = `https://webmaniabr.com/api/1/cep/${cep}/?app_key=kxElXziZEeTKXOMJsykkwAQy398NLtyy&app_secret=pB7tRy8NoJtwScOmXtHYTVfqOi7h8SCIZhg4mlj1GQTubQZn`;
+
+  fetch(url).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (endereco) {
+        document.getElementById("logradouro").value = endereco.endereco;
+        document.getElementById("bairro").value = endereco.bairro;
+        document.getElementById("cidade").value = endereco.cidade;
+        document.getElementById("estado").value = endereco.uf;
+      });
+    }
+  });
+}
